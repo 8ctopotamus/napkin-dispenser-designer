b6a12207e9c043bd185a1d5d68af195f5adb907c
8ctopotamus <joshnaylor88@gmail.com>
2018-08-22 09:21:10 -0500

    added fabric.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <canvas id="c1" width="300" height="300"></canvas>
+
+
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
+import { fabric } from 'fabric'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  mounted () {
+    var canvas = new fabric.Canvas('c1');
+canvas.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 100, left: 100 }));
+
+canvas.selectionColor = 'rgba(0,255,0,0.3)';
+canvas.selectionBorderColor = 'red';
+canvas.selectionLineWidth = 5;
+// this.__canvases.push(canvas);
+
   }
 }
 </script>
@@ -24,5 +38,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+canvas {
+  box-shadow: 0px 0px 3px rgba(0,0,0,.25);
 }
 </style>
