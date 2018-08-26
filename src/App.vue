627c00c45dc81a1643db617f741e5b682df2ad1b
8ctopotamus <joshnaylor88@gmail.com>
2018-08-26 12:01:42 -0500

    Implements addText and addImage

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,82 @@
 <template>
   <div id="app">
-    <canvas id="c1" width="300" height="300"></canvas>
+    <canvas ref="c1" id="c1" width="576" height="384"></canvas>
 
+    <input type="text" v-model="text" />
+    <button @click="addText" type="button" name="add-text">Add text</button>
 
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <select>
+      <option value="Lato">Lato</option>
+      <option value="Pacifico">Pacifico</option>
+    </select>
+
+    <label for="font-size">Font size</label>
+    <input type="number" name="font-size" v-model="fontSize">
+    <input type="range" name="font-size" v-model="fontSize">
+
+    <file-upload @fileChanged="imageObj = $event"></file-upload>
+    <button type="button" name="Add image" @click="addImage">Add image</button>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+
 import { fabric } from 'fabric'
-import HelloWorld from './components/HelloWorld.vue'
+import FileUpload from './components/FileUpload.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FileUpload,
+  },
+  data () {
+    return {
+      text: 'Add text',
+      fontFamily: 'Lato',
+      fontSize: 16,
+      imageObj: ''
+    }
   },
   mounted () {
-    var canvas = new fabric.Canvas('c1');
-canvas.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 100, left: 100 }));
+    const canvas = new fabric.Canvas('c1', { backgroundColor: "white" })
+    this.canvas = canvas
 
-canvas.selectionColor = 'rgba(0,255,0,0.3)';
-canvas.selectionBorderColor = 'red';
-canvas.selectionLineWidth = 5;
-// this.__canvases.push(canvas);
+    canvas.selectionColor = 'rgba(0,255,0,0.3)';
+    canvas.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 100, left: 100 }));
+    canvas.selectionBorderColor = 'red';
+    canvas.selectionLineWidth = 5;
 
+  },
+  methods: {
+    addImage () {
+      new fabric.Image.fromURL(this.imageObj, (img) => {
+        this.canvas.add(img)
+      })
+    },
+    addText () {
+      this.canvas.add(new fabric.IText(this.text, {
+        fontFamily: this.fontFamily,
+        left: 100,
+        top: 100
+      }))
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  margin-bottom: 60px;
 }
 canvas {
   box-shadow: 0px 0px 3px rgba(0,0,0,.25);
+}
+button {
+  background: #006245;
+  color: #fff;
 }
 </style>
