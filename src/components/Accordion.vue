e7930db1d00214042ad517278ef405270e080daf
8ctopotamus <joshnaylor88@gmail.com>
2018-09-20 12:00:32 -0500

    Flipping chevrons for accordions

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="accordion">
-    <h3 class="accordion-title" @click="open = !open">{{title}}</h3>
-    <i class="fas fa-chevron-circle-down"></i>
+    <h3 class="accordion-title" @click="open = !open">
+      {{title}}
+      <i :class="['fas fa-chevron-circle-down', { 'flipped' : open }]"></i>
+    </h3>
     <transition name="slide-fade">
       <div class="accordion-content" v-show="open">
         <slot></slot>
@@ -22,15 +24,10 @@ export default {
 </script>
 
 <style>
-  /* Enter and leave animations can use different */
-  /* durations and timing functions.              */
   .slide-fade-enter-active,
   .slide-fade-leave-active {
     transition: all .3s ease-out;
   }
-  /* .slide-fade-leave-active {
-    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-  } */
   .slide-fade-enter, .slide-fade-leave-to {
     transform: translateY(-10px);
     opacity: 0;
@@ -44,12 +41,17 @@ export default {
     margin: 0;
     padding: 10px;
   }
+  .accordion-title i {
+    float: right;
+  }
+  .accordion-title i.flipped {
+    transform: rotate(180deg);
+  }
   .accordion-title:hover {
     cursor: pointer;
   }
   .accordion-content {
     border: 1px solid #006245;
-    /* display: none; */
     padding: 12px;
   }
   .accordion-content.open {
